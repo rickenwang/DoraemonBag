9318aa8919262169d50870c72b2e0edc0b619c29
rickenwang <1059310816@qq.com>
2018-10-13 16:07:41 +0800

    update

--- samples/TouchEventDispatch/app/src/main/java/com/doraemon/sample/toucheventdispatch/MainActivity.java
+++ samples/TouchEventDispatch/app/src/main/java/com/doraemon/sample/toucheventdispatch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.doraemon.sample.toucheventdispatch;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Activity
     }
 
     @Override
